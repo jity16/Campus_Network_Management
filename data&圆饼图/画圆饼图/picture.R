252d215ae72083d86b3a5fc3dccfc6e2e1242bc5
jity16 <jity16@mails.tsinghua.edu.cn>
2018-12-22 18:51:25 +0800

    data and graph

---
+++ data&圆饼图/画圆饼图/picture.R
@@ -0,0 +1,17 @@
+library(networkD3)
+rsplit <- function(x) {
+  x <- x[!is.na(x[,1]),,drop=FALSE]
+  if(nrow(x)==0) return(NULL)
+  if(ncol(x)==1) return(lapply(x[,1], function(v) list(name=v)))
+  s <- split(x[,-1, drop=FALSE], x[,1])
+  unname(mapply(function(v,n) {if(!is.null(v)) list(name=n, children=v) else list(name=n)}, lapply(s, rsplit), names(s), SIMPLIFY=FALSE))
+}
+
+raw = read.csv("resource classification-2.csv", stringsAsFactors = F)
+raw[raw == ""] = NA
+#View(raw)
+
+
+
+re = rsplit(raw)[[1]]
+radialNetwork(List = re, fontSize = 9)
